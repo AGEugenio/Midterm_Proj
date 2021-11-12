7ee02d1a307a02f99567274af2d677e374a4061d
GelNio <89887787+GelNio@users.noreply.github.com>
2021-11-12 12:05:30 +0800

    Web App tempdir files

---
+++ tempdir/coconut.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import request
+from flask import render_template, send_file
+coconut = Flask(__name__)
+
+@coconut.route("/")
+def main():
+    return render_template("index.html")
+
+
+@coconut.route("/project")
+def project():
+    return render_template("info.html")
+
+@coconut.route("/about")
+def about():
+    return render_template("about.html")
+#@coconut.route("/coconut.png")
+#def get_image():
+#    filename='coconut.png'
+#    return send_file(filename,mimetype='image/png')
+   
+if __name__ == "__main__":
+    coconut.run(host="0.0.0.0", port=8080)
